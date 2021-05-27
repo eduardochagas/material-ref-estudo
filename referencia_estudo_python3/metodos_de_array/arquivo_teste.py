fc93be3a4e930bff75ebbec04207c9b438fff1c0
eduardo <contatoeduardoch@gmail.com>
2021-05-27 09:19:54 -0300

    adicionado todos os meus novos arquivos de referência python3 (mais organizado)

--- referencia_estudo_python3/metodos_de_array/arquivo_teste.py
+++
@@ -1,6 +0,0 @@
-# arquivo para testar os metodos de array
-
-array = ['calabresa', 'macarrao', 'frango', 'calabresa', 'salada']
-
-array.append('churrasco')
-print(array)
--- referencia_estudo_python3/referencias_de_funcoes/funcao_fibonacci.py
+++
@@ -1,8 +0,0 @@
-
-def fibonacci(numero):
-    if numero <= 1:
-        return numero
-    else:
-        return fibonacci(numero-1) + fibonacci(numero-2)
-
-print(fibonacci(11))
